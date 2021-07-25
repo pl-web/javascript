a6864ebb347e1524c894f69d0d06e965997a8e3a
it-reader <hnature@126.com>
2021-07-26 00:15:16 +0800

    RHS-LHS

---
+++ don't-konw-js/RHS-LHS/index.js
@@ -0,0 +1,13 @@
+// for value number<1> find a varible
+var a = 1; // LHS
+
+// find value
+console.log( a ); // RHS
+
+// example
+function foo(argument) {
+  console.log( argument ); // RHS
+};
+
+foo( 1 ); // RHS and LHS( var argument = 1 )
+
