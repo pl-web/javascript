e247f6a7d4e62a5829201e11aef9daac107c8e78
it-reader <hnature@126.com>
2021-08-20 23:26:38 +0800

    the-good-part

---
+++ js-the-good-parts/units/function.js
@@ -0,0 +1,7 @@
+// Function
+console.log('---------------------------');
+console.log('function-unit');
+// --------------------------
+console.log(Function);
+const foo = () => {};
+console.log(foo === Function.prototype);
---
+++ js-the-good-parts/units/object.js
@@ -0,0 +1,8 @@
+// Object
+console.log('---------------------------');
+console.log('obejct-unit');
+// ---------------------------
+console.log(Object);
+// ...
+const obj = {}; // 字面量创建对象
+console.log(obj.__proto__ === Object.prototype); // true
