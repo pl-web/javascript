e4addb61cdae52c1c939cd34c2bcee6949d9ac94
it-reader <hnature@126.com>
2021-09-07 21:25:22 +0800

    js the good parts

--- js-the-good-parts/units/function.js
+++ js-the-good-parts/units/function.js
@@ -5,3 +5,21 @@ console.log('function-unit');
 console.log(Function);
 const foo = () => {};
 console.log(foo === Function.prototype);
+
+// Invocation Pattern
+
+// The Method Invocation Pattern
+const folwer = {
+  name: 'peach flower',
+  season: 'spring and summer',
+  getFlowerName() {
+    return this.name;
+  }
+};
+
+folwer.getFlowerName();
+
+
+
+// The Function Invocation Pattern
+const str = new String
